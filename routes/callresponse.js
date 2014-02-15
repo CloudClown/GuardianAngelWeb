71231c4c6b52b5222cd1d30365000365dc074950
Robert Nguyen <roberttnb@gmail.com>
2014-02-15 10:22:50 -0500

    Fixing bug in twilio informCall

--- routes/callresponse.js
+++ routes/callresponse.js
@@ -3,6 +3,6 @@ exports.response = function(req,res) {
   var resp = new twilio.TwimlResponse();
   resp.say({voice:'alice'}, 'Emergency!, someone you know is in danger.  Please contact help.');
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.set('Content-Type','text/xml');
   res.end(resp.toString());
 };

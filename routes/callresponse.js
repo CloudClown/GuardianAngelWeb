02989a6c9641d1e28a6c8ee3b815f1dd2e2d44c9
Robert Nguyen <roberttnb@gmail.com>
2014-02-15 10:12:18 -0500

    Changed Twilio endpoint, added dynamic name

--- routes/callresponse.js
+++ routes/callresponse.js
@@ -1,8 +1,7 @@
 var twilio = require('twilio');
 exports.response = function(req,res) {
   var resp = new twilio.TwimlResponse();
-  var name = req.params.name;
-  resp.say({voice:'alice'}, 'Emergency!, '+name+' is in danger.  Please contact help.');
+  resp.say({voice:'alice'}, 'Emergency!, someone you know is in danger.  Please contact help.');
 
   res.set('content-type', 'text/xml');
   res.send(resp.toString());
